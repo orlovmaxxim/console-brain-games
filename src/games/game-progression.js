7ad4fecd2050b822a0237d7e77ce20ba092882d3
Orlov Max <orlov.maxxim@gmail.com>
2018-02-16 13:47:04 +0300

    add new game - missing number in progression

---
+++ src/games/game-progression.js
@@ -0,0 +1,36 @@
+import { cons } from 'hexlet-pairs';
+import { letsPlay, getNumRandom } from '..';
+
+const gameRules = 'What number is missing in this progression?';
+const quantity = 3;
+
+const makeProgression = (start, step, lengthProgression = 10) => {
+  const iterator = (counter, acc) => {
+    if (counter === 0) {
+      return acc;
+    }
+    const nextValue = acc[acc.length - 1] + step;
+    const newAcc = acc.concat(nextValue);
+    return iterator(counter - 1, newAcc);
+  };
+  const arrProgres = [];
+  arrProgres.push(start);
+  return iterator(lengthProgression, arrProgres);
+};
+
+const progressionGame = () => {
+  const startDigitProgression = getNumRandom(1, 50);
+  const progressionStep = getNumRandom(1, 5);
+  const progression = makeProgression(startDigitProgression, progressionStep);
+  const indexAnswer = getNumRandom(0, progression.length - 1);
+  const answer = progression[indexAnswer];
+
+  progression.splice(indexAnswer, 1, '..');
+
+  const res = cons(progression.join(' '), answer.toString());
+  return res;
+};
+
+const playProgression = () => letsPlay(progressionGame, gameRules, quantity);
+
+export default playProgression;
