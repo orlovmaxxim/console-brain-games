cb9b360ca1a49774ff5a924fb64a198e4c101902
Orlov Max <orlov.maxxim@gmail.com>
2018-02-16 14:35:38 +0300

    add new game - prime or not prime

---
+++ src/games/game-prime.js
@@ -0,0 +1,29 @@
+import { cons } from 'hexlet-pairs';
+import { letsPlay, getNumRandom } from '..';
+
+const gameRules = 'Is this number prime?';
+const quantity = 3;
+
+const isNumPrime = (questionNum) => {
+  const iterator = (counter, acc) => {
+    if (counter === acc) {
+      return 1;
+    }
+    if (counter % acc === 0) {
+      return 0;
+    }
+    return iterator(counter, acc + 1);
+  };
+  return iterator(questionNum, 2);
+};
+
+const primeGame = () => {
+  const question = getNumRandom(1, 100);
+  const answer = isNumPrime(question) ? 'yes' : 'no';
+  const res = cons(question, answer);
+  return res;
+};
+
+const playPrime = () => letsPlay(primeGame, gameRules, quantity);
+
+export default playPrime;
